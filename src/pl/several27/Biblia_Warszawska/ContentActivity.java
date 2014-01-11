2458cf64d5a0b82deac2093cf1269236ba3061d0
Maciej Szpakowski <maciek.szpak27@gmail.com>
2014-01-11 09:41:01 +0100

    Add buttons to move from chapters to chapters, from TextView.

--- src/pl/several27/Biblia_Warszawska/ContentActivity.java
+++ src/pl/several27/Biblia_Warszawska/ContentActivity.java
@@ -1,10 +1,14 @@
 package pl.several27.Biblia_Warszawska;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
@@ -33,5 +37,50 @@ public class ContentActivity extends Activity
 		textView.setTextSize(Float.parseFloat(sharedPreferences.getString("settings_fonts", "15")));
 		Log.d("settings_font", sharedPreferences.getString("settings_fonts", "NULL"));
 		textView.setText(text);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.content_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem menuItem)
+	{
+		Intent intent = getIntent();
+		switch(menuItem.getItemId())
+		{
+			case R.id.action_back:
+				if(MainActivity.chapter > 1)
+				{
+					MainActivity.chapter--;
+				}
+				else if(MainActivity.book > 1)
+				{
+					MainActivity.book--;
+					MainActivity.chapter = MainActivity.database.countChapters(MainActivity.book);
+				}
+				finish();
+				startActivity(intent);
+				return true;
+			case R.id.action_forward:
+				if(MainActivity.chapter < MainActivity.database.countChapters(MainActivity.book))
+				{
+					MainActivity.chapter++;
+				}
+				else if(MainActivity.book < MainActivity.database.booksList.length)
+				{
+					MainActivity.book++;
+					MainActivity.chapter = 1;
+				}
+				finish();
+				startActivity(intent);
+				return true;
+			default:
+				return super.onOptionsItemSelected(menuItem);
+		}
 	}
 }
